0e32ac3a379e74d9e0766fc4289ebf75890ab59d
satomeister <klimarcuktaras@ukr.net>
2021-03-17 17:16:36 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import { interval, Subject } from "rxjs";
+import {
+  filter,
+  map,
+  pairwise,
+  takeUntil,
+} from "rxjs/operators";
 
-function App() {
+import './App.css'
+
+const parseTime = (seconds) => {
+  return new Date(seconds).toISOString().slice(11, 19)
+}
+
+const doubleClick$ = new Subject().pipe(
+  map(() => new Date().getTime()),
+  pairwise(),
+  filter((timestamps) => timestamps[0] > new Date().getTime() - 200)
+)
+
+export default function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isCounting, setIsCounting] = useState(false)
+
+  useEffect(() => {
+    const sub = doubleClick$.subscribe(() => {
+      setIsCounting(false)
+    })
+    return () => {
+      sub.unsubscribe()
+    }
+  }, [])
+
+  useEffect(() => {
+    const unsubscribe$ = new Subject();
+    interval(1000)
+      .pipe(
+        takeUntil(unsubscribe$)
+      )
+      .subscribe(() => {
+        if (isCounting) {
+          setSeconds(prev => prev + 1000)
+        }
+      })
+    return () => {
+      unsubscribe$.next();
+      unsubscribe$.complete();
+    };
+  }, [isCounting])
+
+  const start = () => {
+    setIsCounting(true)
+  }
+
+  const stop = () => {
+    setIsCounting(false)
+    setSeconds(0)
+  }
+
+  const reset = () => {
+    setSeconds(0)
+    setIsCounting(true)
+  }
+
+  const wait = () => {
+    doubleClick$.next()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='stopwatch'>
+      <span className='time'>{parseTime(seconds)}</span>
+      <div>
+        <button className="button" onClick={start}>Start</button>
+        <button className="button" onClick={stop}>Stop</button>
+        <button className="button" onClick={reset}>Reset</button>
+        <button className="button" onClick={wait}>Wait</button>
+      </div>
     </div>
   );
 }
-
-export default App;
